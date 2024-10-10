d430d54ef8771a1a274e3adcf904600ddd20ab66
Jaun-22 <joaopgl2000@gmail.com>
2024-10-09 23:27:03 -0300

    primeiro commit

---
+++ src/aplicacao/Main.java
@@ -0,0 +1,26 @@
+package aplicacao;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String nome = "João Pedro";
+        System.out.println("Olá " + nome);
+
+        Long numeroLong = 100L;
+        System.out.println(50L + numeroLong);
+
+        Integer numeroInt = 150;
+        System.out.println(200 + numeroInt);
+
+        Double numeroDoub = 25.00;
+        System.out.println(13.00 + numeroDoub);
+
+        Boolean quintaFeira = false;
+        System.out.println("Hoje é quinta? " +nome +" xuxuzinho "+ quintaFeira);
+
+    }
+
+
+}
